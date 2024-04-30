49a3d73b6e9e91855790f87d73d679bc5d8e1be6
Caio Camargo <caiocbahia@hotmail.com>
2024-04-29 21:12:56 -0300

    added Department's TotalSales function

--- Models/Department.cs
+++ Models/Department.cs
@@ -14,5 +14,15 @@
             Id = id;
             Name = name;
         }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
