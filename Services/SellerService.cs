48781ec60ef83ebbc046dd9e2d07f18389c4c7bf
Caio Camargo <caiocbahia@hotmail.com>
2024-05-20 15:27:37 -0300

    add Update on Service and Edit on controller

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -36,5 +37,26 @@ namespace SalesWebMVC.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found.");
+            }
+			try
+			{
+				_context.Update(obj);
+				_context.SaveChanges();
+			}
+
+			//Essa dinâmica abaixo serve para interceptarmos requisições no nível de acesso de dados
+			//e as exibimos ao nível da camada de serviço
+			//Daí o controller vai lidar apenas com exceções da camada de serviço
+			catch (DbUpdateConcurrencyException e)
+			{
+				throw new DbConcurrencyException(e.Message);
+			}
+		}
     }
 }
